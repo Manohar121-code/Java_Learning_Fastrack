dba6fe0e9b0f3b50c6fae7d119d3165f4729af67
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2021-01-28 18:24:07 +0530

    pattern6 and 7

--- src/basics/patternProgramming/Pattern6.java
+++ src/basics/patternProgramming/Pattern6.java
@@ -1,10 +1,31 @@
 package basics.patternProgramming;
 
+import java.util.Scanner;
+
 public class Pattern6 {
 
-//12345
-// 2345
-//  345
-//   45
-//    5
+//12345 - i = 1, space = 0
+// 2345 - i = 2, space = 1
+//  345 - i = 3, space = 2
+//   45 - i = 4, space = 3
+//    5 - i = 5, space = 4
+
+	public static void main(String[] args) {
+		System.out.println("enter a digit:");
+		int n = new Scanner(System.in).nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			
+			for (int j = 1; j <= i-1; j++) {
+				System.out.print(" ");
+			}
+			
+			for (int j = i; j <= n; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+	}
+
 }
---
+++ src/basics/patternProgramming/Pattern7.java
@@ -0,0 +1,43 @@
+package basics.patternProgramming;
+
+import java.util.Scanner;
+
+public class Pattern7 {
+	// n = 5
+//    *      i = 1  space = 4  star = 1(2*i - 1)
+//   ***     i = 2  space = 3  star = 3
+//  *****    i = 3  space = 2  star = 5
+// *******   i = 4  space = 1  star = 7
+//*********  i = 5  space = 0  star = 9
+	public static void main(String[] args) {
+		System.out.println("enter a digit:");
+		int n = new Scanner(System.in).nextInt();
+		
+		//rows
+		for (int i = 1; i <= n; i++) {
+			
+			//spaces
+			for (int j = 1; j <= (n-i); j++) {
+				System.out.print(" ");
+			}
+			
+			//stars
+			for (int j = 1; j <= ((2 * i) - 1); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	
+//    *
+//   ***
+//  *****
+// *******
+//*********
+// *******
+//  *****
+//	 ***
+//	  *
+	
+}
